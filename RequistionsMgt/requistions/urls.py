bdc1fe96b657ee385713062ada3e4373f7f7f4ad
aldo okware <aldo.okware@moap.co.ug>
2020-07-11 20:57:03 +0300

    chore: code cleanup / refactor

--- RequistionsMgt/requistions/urls.py
+++ RequistionsMgt/requistions/urls.py
@@ -4,8 +4,8 @@ app_name='requistions'
 urlpatterns=[
 
 path('',views.index,name='index'),
-path('requistions/',views.requistion_list,name='requistion_list'),
-path('requistions/<int:pk>',views.requistion_get,name='requistion_get'),
+path('requistions/',views.requisition_list,name='requistion_list'),
+path('requistions/<int:pk>',views.get_requisition,name='requistion_get'),
 path('add_requistion/',views.new_requistion,name='new_requistion'),
 path('requistion_detail/<int:requistion_id>',views.add_details,name='add_details')
 
